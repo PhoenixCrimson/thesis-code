e8423ceb0f1d6466c8b51824d95e1fd002ae4317
PhoenixCrimson <33698159+PhoenixCrimson@users.noreply.github.com>
2023-06-02 12:35:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ class Block:
         return Block(minkowski_difference_base_point, minkowski_difference_body_diagonal)
 
 
-class DOMB:
+class MultiBlock:
     def __init__(self, base_block=None, mark_block=None, group=None, subgroup=None):
         self.block_list = []
         self.base_block = Block(base_block[0], base_block[1])
@@ -275,8 +275,9 @@ class DOMB:
                 Total_Minkowski_difference.add(volumeElement)
         return len(Total_Minkowski_difference)
 
-    def MonteCarloExcludedVolumes(self, n=10**9):
-        Length = self.base_block.greatest_length + self.mark_block.greatest_length
+    def MonteCarloExcludedVolumes(self, n=10**6):
+        Length = 2*(self.base_block.greatest_length + self.mark_block.greatest_length)
+        print(Length)
         k = 0
         for i in range(n):
             Translation = [random.uniform(-Length, Length), random.uniform(-Length, Length), random.uniform(-Length, Length)]
@@ -305,26 +306,25 @@ class DOMB:
             self.reorient(element)
             Reoriented_Ranges = [block.ranges for block in self.orientable_block_list]
             Reoriented_base = Reoriented_Ranges.pop(0)
-            if Base == Reoriented_base:
+            Unoriented_Base = self.base_block
 
-
-                check = all(block in Reoriented_Ranges for block in Block_Ranges)
-                if element in self.subgroup.elements.values():
-                    if check is True:
-                        #print("Valid MultiBlock")
-                        i += 1
-                    else:
-                        print(f"Invalid for {element.right_hand_label},\n{[Block_Ranges[0], Reoriented_Ranges]}")
+            check1 = Unoriented_Base.ranges == Reoriented_base
+            check2 = all(block in Reoriented_Ranges for block in Block_Ranges)
+            if element in self.subgroup.elements.values():
+                if (check1 and check2) is True:
+                    #print("Valid MultiBlock")
+                    i += 1
                 else:
-                    if check is False:
-                        #print("Valid Multiblock")
-                        i += 1
-                    else:
-                        print(f"Invalid for {element.right_hand_label},\n{[Block_Ranges, Reoriented_Ranges]}")
+                    print(f"Invalid for {element.right_hand_label},\n{[Block_Ranges[0], Reoriented_Ranges]}")
             else:
-                i += 1
-            if i == n:
-                print("Validation complete: Multiblock is valid.")
+                if (check1 and check2) is False:
+                    #print("Valid Multiblock")
+                    i += 1
+                else:
+                    print(f"Invalid for {element.right_hand_label},\n{[Block_Ranges, Reoriented_Ranges]}")
+
+        if i == n:
+            print("Validation complete: Multiblock is valid.")
 
 PlusMinusOne = [1, -1]
 
@@ -394,23 +394,7 @@ Subgroup_Label_Dict = {"Oh": ["(0,0)", "(0,1)", "(0,2)", "(0,3)", "(0,4)", "(0,5
                                 "(5,1)",
                                 "(6,1)",
                                 "(7,0)"],
-                       "D4_1": ["(0,0)", "(0,1)",
-                                "(1,0)", "(1,1)",
-                                "(2,0)", "(2,1)",
-                                "(3,0)", "(3,1)"],
-                       "D4_2": ["(0,0)",
-                                "(1,0)",
-                                "(2,0)",
-                                "(3,0)",
-                                "(4,1)",
-                                "(5,1)",
-                                "(6,1)",
-                                "(7,1)"],
-                       "D4_3": ["(0,0)", "(0,1)",
-                                "(3,0)", "(3,1)",
-                                "(5,0)", "(5,1)",
-                                "(6,0)", "(6,1)"],
-                       "D4_4": ["(0,0)",
+                       "D4_1": ["(0,0)",
                                 "(1,1)",
                                 "(2,1)",
                                 "(3,0)",
@@ -418,39 +402,61 @@ Subgroup_Label_Dict = {"Oh": ["(0,0)", "(0,1)", "(0,2)", "(0,3)", "(0,4)", "(0,5
                                 "(5,0)",
                                 "(6,0)",
                                 "(7,1)"],
-                       "D3_1": ["(0,0)", "(0,1)", "(0,2)", "(0,3)", "(0,4)", "(0,5)"],
-                       "D3_2": ["(0,0)", "(7,1)", "(7,2)", "(0,3)", "(0,4)", "(7,5)"],
+                       "D4_2": ["(0,0)", "(0,1)",
+                                "(1,0)", "(1,1)",
+                                "(2,0)", "(2,1)",
+                                "(3,0)", "(3,1)"],
+                       "D4_3": ["(0,0)",
+                                "(1,0)",
+                                "(2,0)",
+                                "(3,0)",
+                                "(4,1)",
+                                "(5,1)",
+                                "(6,1)",
+                                "(7,1)"],
+                       "D4_4": ["(0,0)", "(0,1)",
+                                "(3,0)", "(3,1)",
+                                "(5,0)", "(5,1)",
+                                "(6,0)", "(6,1)"],
+
+                       "D3_1": ["(0,0)", "(7,1)", "(7,2)", "(0,3)", "(0,4)", "(7,5)"],
+                       "D3_2": ["(0,0)", "(0,1)", "(0,2)", "(0,3)", "(0,4)", "(0,5)"],
+
                        "C6": ["(0,0)",  "(0,3)", "(0,4)", "(7,0)",  "(7,3)", "(7,4)"],
                        "C4_1": ["(0,0)",
-                                "(3,0)",
-                                "(5,1)",
-                                "(6,1)"],
-                       "C4_2": ["(0,0)",
                                 "(1,1)",
                                 "(2,1)",
                                 "(3,0)"],
-                       "K4_1": ["(0,0)",
+                       "C4_2": ["(0,0)",
+                                "(3,0)",
+                                "(5,1)",
+                                "(6,1)"],
+                       "K4_1": ["(0,0)", "(3,0)",
+                                "(5,0)", "(6,0)"],
+
+                       "K4_2": ["(0,0)", "(3,0)",
+                                "(4,1)", "(7,1)"],
+                       "K4_3": ["(0,0)", "(1,0)",
+                                "(2,0)", "(3,0)"],
+                       "K4_4": ["(0,0)", "(0,1)",
+                                "(3,0)", "(3,1)"],
+                       "K4_5": ["(0,0)", "(0,1)",
+                                "(4,0)", "(4,1)"],
+
+                       "K4_6": ["(0,0)",
                                 "(3,0)",
                                 "(4,0)",
                                 "(7,0)"],
-                       "K4_2": ["(0,0)", "(0,1)",
+                       "K4_7": ["(0,0)", "(0,1)",
                                 "(7,0)", "(7,1)"],
-                       "K4_3": ["(0,0)", "(0,1)",
-                                "(4,0)", "(4,1)"],
-                       "K4_4": ["(0,0)", "(1,0)",
-                                "(2,0)", "(3,0)"],
-                       "K4_5": ["(0,0)", "(0,1)",
-                                "(3,0)", "(3,1)"],
-                       "K4_6": ["(0,0)", "(3,0)",
-                                "(4,1)", "(7,1)"],
-                       "K4_7": ["(0,0)", "(3,0)",
-                                "(5,0)", "(6,0)"],
+
                        "C3": ["(0,0)", "(0,3)", "(0,4)"],
-                       "C2_1": ["(0,0)", "(7,0)"],
-                       "C2_2": ["(0,0)", "(4,0)"],
-                       "C2_3": ["(0,0)", "(0,1)"],
-                       "C2_4": ["(0,0)", "(3,0)"],
-                       "C2_5": ["(0,0)", "(7,1)"],
+                       "C2_1": ["(0,0)", "(3,0)"],
+                       "C2_2": ["(0,0)", "(7,1)"],
+                       "C2_3": ["(0,0)", "(4,0)"],
+                       "C2_4": ["(0,0)", "(0,1)"],
+                       "C2_5": ["(0,0)", "(7,0)"],
+
                        "C1": ["(0,0)"]
 
 
@@ -463,38 +469,116 @@ for label, subgroup in Subgroup_Dict.items():
 
 
 #Parameter settings
-a = 1
-b = 1
-c = 3
+a = 2
+b = 3
+c = 4
 d = 1
 e = 2
-f = 1
-Subgroup = "D4C2"
+f = 3
+Subgroup = "O"
 
 Full_Octahedral_Group.findCoset(Full_Octahedral_Group.subgroupDict[Subgroup])
 
-Base_block = [[-a, -b, -c], [2*a, 2*b, 2*c]]
-Marker_block = None
-D = DOMB(Base_block, Marker_block, group=Full_Octahedral_Group, subgroup=Subgroup_Dict[Subgroup])
-D.verifyGroupExclusivity()
+Base_Block_Dict = {"Bc": [[-a, -a, -a], [2*a, 2*a, 2*a]], "Bs": [[-a, -a, -c], [2*a, 2*a, 2*c]], "Br": [[-a, -b, -c], [2*a, 2*b, 2*c]]}
+Marker_Block_Dict = {"Bcs": [[-d,-d,c], [2*d, 2*d, 2*f]], "Bcr": [[-d,-e,c], [2*d, 2*e, 2*f]], 'Best': [[-d,b,c], [2*d, -2*d, 2*f]],
+                     'Bert': [[-d,b,c], [2*d, -2*e, 2*f]], 'Bess': [[-d,b,c], [2*d, 2*d, -2*f]], 'Bers': [[-d,b,c], [2*d, 2*e, -2*f]],
+                     'Bvst': [[a,b,c], [-2*d, -2*d, 2*f]], 'Bvrt': [[a,b,c], [-2*d, -2*e, 2*f]], 'Bvss': [[a,b,c], [-2*d, 2*d, -2*f]],
+                     'Bvrs': [[a,b,c], [-2*d, 2*e, -2*f]]
+                     }
 
-D.reorient(D.group.elements["(0,4)"])
-D.getMinkowskiDifferences()
+Multiblock_Dict = {"Oh": MultiBlock(Base_Block_Dict["Bc"], None, Full_Octahedral_Group, Subgroup_Dict['Oh']),
+                   "O":  MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group, Subgroup_Dict['O']),
+                   "S4":  MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bvst'], Full_Octahedral_Group, Subgroup_Dict['S4']),
+                   "A4C2": MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bcr'], Full_Octahedral_Group, Subgroup_Dict['A4C2']),
+                   "D4C2": MultiBlock(Base_Block_Dict["Bs"], None, Full_Octahedral_Group,
+                                      Subgroup_Dict['D4C2']),
+                   "A4": MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['A4']),
+                   "D6": MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bvst'], Full_Octahedral_Group,
+                                      Subgroup_Dict['D6']),
+                   "C23_1": MultiBlock(Base_Block_Dict["Br"], None, Full_Octahedral_Group,
+                                      Subgroup_Dict['C23_1']),
+                   "C23_2": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C23_2']),
+                   "C4C2": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C4C2']),
+                   "D4_1": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['D4_1']),
+                   "D4_2": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bcs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['D4_2']),
+                   "D4_3": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bert'], Full_Octahedral_Group,
+                                      Subgroup_Dict['D4_3']),
+                   "D4_4": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvst'], Full_Octahedral_Group,
+                                      Subgroup_Dict['D4_4']),
+                   "C6": MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C6']),
+                   "D3_1": MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['D3_1']),
+                   "D3_2": MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bvst'], Full_Octahedral_Group,
+                                      Subgroup_Dict['D3_2']),
+                   "C4_1": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C4_1']),
+                   "C4_2": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C4_2']),
+                   "K4_1": MultiBlock(Base_Block_Dict["Br"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['K4_1']),
+                   "K4_2": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['K4_2']),
+                   "K4_3": MultiBlock(Base_Block_Dict["Br"], Marker_Block_Dict['Bcr'], Full_Octahedral_Group,
+                                      Subgroup_Dict['K4_3']),
+                   "K4_4": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['K4_4']),
+                   "K4_5": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvst'], Full_Octahedral_Group,
+                                      Subgroup_Dict['K4_5']),
+                   "K4_6": MultiBlock(Base_Block_Dict["Br"], Marker_Block_Dict['Bert'], Full_Octahedral_Group,
+                                      Subgroup_Dict['K4_6']),
+                   "K4_7": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['K4_7']),
+                   "C3": MultiBlock(Base_Block_Dict["Bc"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C3']),
+                   "C2_1": MultiBlock(Base_Block_Dict["Br"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['A4C2']),
+                   "C2_2": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C2_2']),
+                   "C2_3": MultiBlock(Base_Block_Dict["Br"], Marker_Block_Dict['Bcr'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C2_3']),
+                   "C2_4": MultiBlock(Base_Block_Dict["Bs"], Marker_Block_Dict['Bvrs'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C2_4']),
+                   "C2_5": MultiBlock(Base_Block_Dict["Br"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C2_5']),
+                   "C1": MultiBlock(Base_Block_Dict["Br"], Marker_Block_Dict['Bvrt'], Full_Octahedral_Group,
+                                      Subgroup_Dict['C1'])
+
+                   }
+
+Base_block = [[-a, -b, -c], [2*a, 2*b, 2*c]]
+Marker_block = [[-d,-e,c], [2*d, 2*e, 2*f]]
+D = Multiblock_Dict[Subgroup]
+D.verifyGroupExclusivity()
+for element in D.group.cosetDict[Subgroup]:
+    print(f"Coset {element.left_hand_label}")
+    for element2 in D.group.subgroupDict[Subgroup].elements.values():
+        element3 = element*element2
+        print(element3.left_hand_label)
+D.getMinkowskiDifferences(D.group.elements["(4,1)"])
 for Difference in D.minkowski_difference_dictionary.keys():
     print(Difference, "-->", D.minkowski_difference_dictionary[Difference])
 D.reorient(D.group.elements["(0,0)"])
 
 D.reorient(D.group.elements["(0,0)"])
 EV = D.calculateExcludedVolumes()
-for Label, Volume in EV.items():
-    print(f"{Label}: {Volume}")
+# for Label, Volume in EV.items():
+#     print(f"{Label}: {Volume}")
 D.reorient(D.group.elements["(0,0)"])
 cosets = list(D.group.cosetDict[Subgroup])
-for c_index in range(len(cosets)):
-    D.reorient(cosets[c_index])
-    D.getMinkowskiDifferences(cosets[c_index])
-    MCvolume = D.MonteCarloExcludedVolumes(n=10**6)
-    Tev = D.getExcludedVolume(cosets[c_index])
-    print(f'{cosets[c_index].right_hand_label}, {Tev}')
-    print(MCvolume)
-    print((MCvolume - Tev)/Tev*100, "%")
+D.reorient(D.group.elements["(0,0)"])
+# for c_index in range(len(cosets)):
+#     D.reorient(D.group.neutral)
+#     D.getMinkowskiDifferences(cosets[c_index])
+#     #MCvolume = D.MonteCarloExcludedVolumes(n=10**6)
+#     Tev = D.getExcludedVolume(cosets[c_index])
+#     print(f'{cosets[c_index].right_hand_label}, {Tev}')
+#     #print(MCvolume)
+#     #print((MCvolume - Tev)/Tev*100, "%")
+
+print(D.group.elements["(4,5)"].left_hand_label)
